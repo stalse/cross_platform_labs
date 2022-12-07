b6d0243ad61afa82a66d93c1ce8d82ac549d4360
stalse <stalse644@gmail.com>
2022-12-07 22:10:00 +0200

    laba2

---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace laba2
+{
+    class Program
+    {
+        static public void mnogo(string s, int step, int n)
+        {
+            
+            if(step < 27)
+            {
+                char newletter = (char)(64 + step);
+                
+                if (s != null)
+                {
+                    s = newletter + s + s;
+                }
+                else
+                {
+                    s = newletter.ToString();
+                }
+                mnogo(s, step + 1, n);
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(@"C:\\Users\\ыае\\Desktop\\OUTPUT2.txt"))
+                {
+                    sw.WriteLine(s[n-1]);
+                };
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] text = System.IO.File.ReadAllLines(@"C:\\Users\\ыае\\Desktop\\INPUT2.txt");
+            List<int> intset = new List<int>();
+
+            try
+
+            {
+
+                foreach (string s in text)
+                {
+                    intset.Add(Int32.Parse(s));
+
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Введіть число!");
+            }
+
+
+
+            mnogo("", 1, intset[0]);
+        }
+    }
+}
