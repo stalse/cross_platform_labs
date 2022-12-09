f2c8cc4bf77c3f05691c8682a5a882b8628ee16a
stalse <stalse644@gmail.com>
2022-12-09 22:11:55 +0200

    laba3

--- laba3/laba3/Program.cs
+++ laba3/laba3/Program.cs
@@ -63,23 +63,93 @@ namespace laba3
                 //    Console.WriteLine("Введіть число!");
                 //}
             }
-            foreach(List<int> y in x)
+
+            //foreach (List<int> y in x)
+            //{
+
+            //    Console.WriteLine();
+            //    foreach (int w in y)
+            //    {
+            //        Console.Write(w + " ");
+            //    }
+            //}
+            List<int> allWeight = new List<int>();
+            
+            for (int i = 0; i < x[s-1].Count; i++)
             {
-                Console.WriteLine();
-                foreach(int w in y)
+                
+                    allWeight.Add(x[s - 1][i]);
+                    
+               
+
+            }
+            int allWeightW = 0;
+            while (true)
+            {
+                int min = 500000000, w = 0;
+
+                for (int i = 0; i < allWeight.Count; i++)
                 {
-                    Console.Write(w + " ");
+                    if (allWeight[i] != -1 && allWeight[i] != 0)
+                    {
+
+                        if (Math.Min(min, allWeight[i]) != min)
+                        {
+                            w = i;
+                        }
+                        min = Math.Min(min, allWeight[i]);
+                    }
+
+
+                }
+                allWeightW = allWeight[w];
+                allWeight[w] = -1;
+                for (int i = 0; i < allWeight.Count; i++)
+                {
+                    if (allWeight[i] != -1 && allWeight[i] != 0)
+                    {
+
+                        for (int j = 0; j < x[w].Count; j++)
+                        {
+                            if (j == i)
+                            {
+                                
+                                allWeight[i] = Math.Min(allWeight[i], allWeightW + x[w][j]);
+                                
+                            }
+
+                        }
+
+                    }
+
+                    if (i == f - 1 && allWeight[i] != -1 && allWeight[i] != 0)
+                    {
+                        allWeightW = allWeight[i];
+                    }
+                }
+                int count = 0;
+                foreach(int r in allWeight)
+                {
+                    
+                    if (r == -1 || r == 0)
+                    {
+                        count++;
+                    }
+                }
+                
+                if (count == allWeight.Count)
+                {
+                    break;
                 }
             }
 
 
 
 
-
-            //using (StreamWriter sw = File.CreateText(@"C:\\Users\\ыае\\Desktop\\OUTPUT.txt"))
-            //{
-            //    sw.WriteLine();
-            //}
+            using (StreamWriter sw = File.CreateText(@"C:\\Users\\ыае\\Desktop\\OUTPUT.txt"))
+            {
+                sw.WriteLine(allWeightW);
+            }
         }
     }
 }
